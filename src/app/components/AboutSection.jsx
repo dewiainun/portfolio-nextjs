9e3fb283b90fad32886111d3810d9a5a2301a75e
Dewi Ainun Amaliah <141829288+dewiainun@users.noreply.github.com>
2024-11-10 18:13:42 +0700

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -23,8 +23,7 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>University of Gunadarma</li>
       </ul>
     ),
   },
@@ -53,7 +52,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+      <Image src="/images/about-image.png" width={500} height={500} alt="Profile image" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
@@ -65,27 +64,25 @@ const AboutSection = () => {
             I am excited to work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
-            </TabButton>
+          <TabButton
+  selectTab={() => handleTabChange("skills")}
+  active={tab === "skills"}
+>
+  Skills
+</TabButton>
+<TabButton
+  selectTab={() => handleTabChange("education")}
+  active={tab === "education"}
+>
+  Education
+</TabButton>
+<TabButton
+  selectTab={() => handleTabChange("certifications")}
+  active={tab === "certifications"}
+>
+  Certifications
+</TabButton>
+
           </div>
           <div className="mt-8">
             {TAB_DATA.find((t) => t.id === tab).content}
